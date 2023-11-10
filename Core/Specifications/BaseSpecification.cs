50fd4f05a915aaf6cd2428ce1a915d1fec4ca7db
CraigWilliams8 <craig.williams@testdrivensolutions.co.uk>
2023-11-10 11:21:58 +0000

    End of Section 4

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -5,13 +5,23 @@ namespace Core.Specifications
 {
     public class BaseSpecification<T> : ISpecification<T>
     {
-        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        public BaseSpecification()
+        {
+        }
+
+        public BaseSpecification(Expression<Func<T, bool>> criteria) 
         {
             Criteria = criteria;
         }
 
         public Expression<Func<T, bool>> Criteria {get; }
 
-        public List<Expression<Func<T, object>>> Includes {get; } = new List<Expression<Func<T, object>>>();
+        public List<Expression<Func<T, object>>> Includes {get; } = 
+        new List<Expression<Func<T, object>>>();
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
     }
 }
