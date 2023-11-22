3518b91a8294d81615bab0df67d3c0f068ac7b46
CraigWilliams8 <craig.williams@testdrivensolutions.co.uk>
2023-11-22 14:55:23 +0000

    ANG-0001: added start sale to service, not sure if it works

--- client/src/app/sale/sale.service.ts
+++ client/src/app/sale/sale.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SaleService {
+  baseUrl = 'https://localhost:5001/api/'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  startSale() 
+  {
+    
+    return this.http.get(this.baseUrl + 'products/start-sale')
+  }
+
+  
 }
