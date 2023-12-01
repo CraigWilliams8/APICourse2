864c20682e855a1b95a53826373e1d43fb38f837
CraigWilliams8 <craig.williams@testdrivensolutions.co.uk>
2023-12-01 13:50:13 +0000

    Halfway through section 18

---
+++ Infrastructure/Services/OrderService.cs
@@ -0,0 +1,66 @@
+
+using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+
+
+namespace Infrastructure.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IGenericRepository<Order> _orderRepo;
+        private readonly IGenericRepository<DeliveryMethod> _dmRepo;
+        private readonly IGenericRepository<Product> _productRepo;
+        private readonly IBasketRepository _basketRepo;
+        public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod>
+        dmRepo, IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
+        {
+            _productRepo = productRepo;
+            _basketRepo = basketRepo;
+            _orderRepo = orderRepo;
+            _dmRepo = dmRepo;
+        }
+
+        public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+        {
+            var basket = await _basketRepo.GetBasketAsync(basketId);
+
+            var items = new List<OrderItem>();
+
+            foreach(var item in basket.Items)
+            {
+                var productItem = await _productRepo.GetByIdAsync(item.Id);
+                var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name,
+                 productItem.PictureUrl);
+                 var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+                 items.Add(orderItem);
+            }
+
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+
+
+            var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
+
+            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+
+            //To DO save to db
+
+            return order;
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
